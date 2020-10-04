bb8bdf32988845deb4cdc9e0a6cdc284d7897583
Han Wen Xie <hanw.xie@gmail.com>
2020-10-04 12:13:32 -0400

    Updates

---
+++ precache-manifest.54e785c6dd519910bbd46fd26aa1e136.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f16fc77d81f6d200b2ebb101b90b128",
+    "url": "/cv-project/index.html"
+  },
+  {
+    "revision": "b3442fb2066a32662da2",
+    "url": "/cv-project/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "446a0fd2b351037c567c",
+    "url": "/cv-project/static/css/main.5c1018f4.chunk.css"
+  },
+  {
+    "revision": "b3442fb2066a32662da2",
+    "url": "/cv-project/static/js/2.ca632c72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv-project/static/js/2.ca632c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "446a0fd2b351037c567c",
+    "url": "/cv-project/static/js/main.faa01c42.chunk.js"
+  },
+  {
+    "revision": "f1ebb3293288666c5950",
+    "url": "/cv-project/static/js/runtime-main.d91c792d.js"
+  }
+]);
